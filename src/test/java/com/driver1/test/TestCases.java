71eae1f7e35dd7c30d2579650b745288df75f5eb
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-06-24 16:33:14 +0530

    check commit 6

--- src/test/java/com/driver1/test/TestCases.java
+++ src/test/java/com/driver1/test/TestCases.java
@@ -1,11 +1,11 @@
 package com.driver1.test;
 
-import com.driver1.AirBnbtemp;
+import com.driver.AirBnb;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = AirBnbtemp.class)
+@SpringBootTest(classes = AirBnb.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestCases {
 
